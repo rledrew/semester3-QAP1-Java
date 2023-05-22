c5855c8828225894a14264a5a446409b7d17d021
Riley <riley.ledrew@keyin.com>
2023-05-21 22:58:06 -0230

    added more testing features

--- TestDate.java
+++ TestDate.java
@@ -2,5 +2,7 @@ public class TestDate {
     public static void main(String[] args) {
         Date today = new Date(1, 5, 2023);
         System.out.println(today.toString());
+        today.setDate(11, 12, 2013);
+        System.out.println(today.toString());
     }
 }
